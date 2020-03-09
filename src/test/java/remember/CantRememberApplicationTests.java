8b4e97f928c5a716de4a72ae612275ee3e7d3fbc
LaVi <lastvirgo266@gmail.com>
2020-03-10 00:16:07 +0900

    Init : Project CantRemember

---
+++ src/test/java/remember/CantRememberApplicationTests.java
@@ -0,0 +1,13 @@
+package remember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CantRememberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
