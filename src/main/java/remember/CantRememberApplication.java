438dc1e6f36fa941eee78466f9623e51a9f12546
LaVi <lastvirgo266@gmail.com>
2020-03-17 17:07:59 +0900

    Revise : kakao.proeprties

--- src/main/java/remember/CantRememberApplication.java
+++ src/main/java/remember/CantRememberApplication.java
@@ -2,8 +2,10 @@ package remember;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("kakao.properties") //개인적으로 만든 프로퍼티 불러오기
 public class CantRememberApplication {
 
 	public static void main(String[] args) {
