65b06f166d650a839e1f55adc3e7d37899619805
LaVi <lastvirgo266@gmail.com>
2020-03-22 21:56:51 +0900

    Delete korean

--- src/main/java/remember/BO/KakaoAPI.java
+++ src/main/java/remember/BO/KakaoAPI.java
@@ -9,7 +9,6 @@ public interface KakaoAPI {
 
 	public List<String> getAccessToken(String authorize_code);
 	
-	//게임의 정보를 카카오 API에 저장시킴
 	public void SaveGameInfoToKakao(String access_Token, GameDto game);
 	
 	public void logout(String access_Token);
